390293e2b4aae5686e9052321c17f599b92471d6
yuliusadp <yuliuscaesar28@gmail.com>
2021-12-19 15:47:41 +0700

    Update Contoh1.php

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Achmad Munawir Ziham
-            Saya tinggal di daerah Jatinegara Kaum
-            Hobby saya bermusik";
+        echo"Nama saya Yulius
+            Saya tinggal di daerah Asrama Yon Ang Air 
+            Hobby saya basket";
     }
 } 
